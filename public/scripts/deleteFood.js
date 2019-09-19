1663dda5215f94e2422d0a75918f39064ceb39ad
decagon-bootcamp <dumpmailnathan@gmail.com>
2019-09-19 16:23:05 +0100

    deleteFood

---
+++ public/scripts/deleteFood.js
@@ -0,0 +1,32 @@
+function deleteFood() {
+    $('.delete').submit(function (e) {
+        e.preventDefault();
+        let foodType = $('#foodType').val();
+        let menu = $('#menu').val();
+        let side = $('#side').val();
+        const price = $('#price').val();
+        const food = { foodType, price, menu, side }
+        const url = $(this).attr('action');
+
+        if (foodType === '') {
+            alert("Please enter Food Type");
+        } else if (menu === '') {
+            alert("Please enter the menu");
+        } else if (side === '') {
+            alert("Please enter the side");
+        } else if (price === '' || price === 0) {
+            alert("Please enter the price");
+        } else {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                data: food,
+                success: function (data) {
+
+                    window.location = '/create.html';
+                }
+            })
+        }
+        return false;
+    })
+}
