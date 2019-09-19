077b902f70d6d591b6a1820939c9106db2ac0705
decagon-bootcamp <dumpmailnathan@gmail.com>
2019-09-19 16:19:56 +0100

    createOrde.js

---
+++ public/scripts/createOrder.js
@@ -0,0 +1,27 @@
+function createOrder() {
+    $('.form').submit(function (e) {
+        e.preventDefault();
+        const price = $('#price').val();
+        let foodType = $('#foodType').val();
+        const location = $('#location').val();
+        const url = $(this).attr('action');
+        const order = { foodType, price, location, userEmail }
+        if (foodType === '') {
+            alert("Please select from the menu");
+        } else if (price === '' || price === 0) {
+            alert("Please enter the price");
+        } else if (location === '') {
+            alert("Please enter your location");
+        } else {
+            $.ajax({
+                url: url,
+                type: 'POST',
+                data: order,
+                success: function (data) {
+                    window.location.replace('./welcome.html');
+                }
+            })
+        }
+        return false;
+    })
+}
