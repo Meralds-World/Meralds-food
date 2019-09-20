a38c10e10562d7f857bf2ab7da86bd0630e1eea3
decagon-bootcamp <dumpmailnathan@gmail.com>
2019-09-20 06:52:53 +0100

    public

--- public/scripts/viewOrder.js
+++ public/scripts/viewOrder.js
@@ -1,29 +1,20 @@
+
 $(document).ready(function () {
-    const userValue = JSON.parse(localStorage.getItem('users'));
-    console.log(userValue)
-    const name = userValue.userName
-    const newEmail = userValue.email;
-    $.ajax({
-        type: 'GET',
-        url: `http://localhost:3000/order/?userEmail=${newEmail}`,
-        dataType: 'json'
-    }).done(function (data) {
-        console.log(data)
-        $('#welcome').html(`<h2>Welcome, ${name}</h2>`)
+    $.get("http://localhost:3000/order", function (data) {
+        console.log(data);
+
         for (let i = 0; i < data.length; i++) {
             if (data.length === 0) {
                 $('#order-table').html('<h2>You have not placed any order</h2>')
             } else {
                 $('.tbody').append(`
                 <tr>
-                  <td>${data[i].id}</td>
-                  <td>${data[i].userEmail}</td>
-                  <td>${data[i].foodType}</td>
-                  <td>${data[i].price}</td>
-                  <td>${data[i].location}</td>
+                <td>${data[i].id}</td>
+                <td>${data[i].foodType}</td>
+                <td>${data[i].price}</td>
+                <td>${data[i].location}</td>
                 </tr>`);
             }
         }
-
     });
-})
+});
